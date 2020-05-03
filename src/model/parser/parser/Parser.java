cfbe6df7dfb0f765c4ee6affd1b78503b2c1e266
harraz21 <mohamedharraz21@gmail.com>
2020-05-03 15:17:12 +0200

    parser tested om working program

--- src/model/parser/parser/Parser.java
+++ src/model/parser/parser/Parser.java
@@ -20,9 +20,7 @@ public class Parser {
 
     private CFG grammar;
 
-    private ArrayList<Pair<String, String>> inputTokens;
-    private HashMap<Integer, String> errors;
-    private ArrayList<Stack<String>> outStacks;
+    private ArrayList<String> inputTokens;
 
 
     private ArrayList<Pair<String, Pair<String, String>>> log;
@@ -33,12 +31,19 @@ public class Parser {
 
     public Parser(ParserGenerator parserGenerator, ArrayList<Pair<String, String>> inputTokens) {
         this.parsingTable = parserGenerator.getParsingTable();
-        this.inputTokens = inputTokens;
+        this.inputTokens = getInputTokens(inputTokens);
         this.grammar = parserGenerator.getGrammar();
-        errors = new HashMap<>();
-        outStacks = new ArrayList<>();
         log = new ArrayList<>();
         parse();
+    }
+
+    private ArrayList<String> getInputTokens(ArrayList<Pair<String, String>> inputTokens) {
+        ArrayList<String> inputTokensOut = new ArrayList<>();
+        for (Pair<String, String> valLex : inputTokens) {
+            inputTokensOut.add(valLex.getValue());
+        }
+        inputTokensOut.add(Constant.END_MARKER);
+        return inputTokensOut;
     }
 
     public void parse() {
@@ -48,7 +53,7 @@ public class Parser {
         stack.push(grammar.getStartingNonTerminal());
         while (!stack.empty() && inputTokenIndex != inputTokens.size()) {
             for (String string : stack) {
-                System.out.print(string + "\t");
+                System.out.print(string + ",\t\t\t");
             }
             System.out.println();
 
@@ -59,7 +64,6 @@ public class Parser {
             if (grammar.isNonTerminal(TOS)) { /** if top of stack is non terminal */
                 /** if top of stack leads to empty entry */
                 if (!parsingTable.get(TOS).containsKey(inputTokens.get(inputTokenIndex))) {
-                    errors.put(inputTokenIndex, inputTokens.get(inputTokenIndex).getValue());
                     inputTokenIndex++;
                     stack.push(TOS);
                     continue;
@@ -70,42 +74,30 @@ public class Parser {
                 }
                 /** if top of stack is production rule */
                 if (parsingTable.get(TOS).get(inputTokens.get(inputTokenIndex)).get(0).get(0).equals(Constant.SYNC_TOK)) {
-                    errors.put(inputTokenIndex, inputTokens.get(inputTokenIndex).getValue());
-                    inputTokenIndex++;
-                    stack.push(TOS);
                     continue;
                 }
                 /** a production rule needs to be pushed to stack */
-                int lengthOfArray = parsingTable.get(TOS).get(inputTokens.get(inputTokenIndex).getValue()).get(0)
-                        .size();
-                for (int i = lengthOfArray - 1; i >= 0; i++) {
-                    stack.push(parsingTable.get(TOS).get(inputTokens.get(inputTokenIndex).getValue()).get(0).get(i));
+                int lengthOfArray = parsingTable.get(TOS).get(inputTokens.get(inputTokenIndex)).get(0).size();
+                for (int i = lengthOfArray - 1; i >= 0; i--) {
+                    stack.push(parsingTable.get(TOS).get(inputTokens.get(inputTokenIndex)).get(0).get(i));
                 }
                 continue;
             } else { /** if top of stack is terminal */
                 /** if input token match top of stack */
-                if (TOS.equals(inputTokens.get(inputTokenIndex).getValue())) {
+                if (TOS.equals(inputTokens.get(inputTokenIndex))) {
                     inputTokenIndex++;
                     continue;
                 }
                 /** if input token doesn't match top of stack */
                 else {
-                    errors.put(inputTokenIndex, inputTokens.get(inputTokenIndex).getValue());
                     inputTokenIndex++;
                 }
             }
         }
+        System.out.println("finished");
     }
 
     public ArrayList<Pair<String, Pair<String, String>>> getLog() {
         return log;
-    }
-
-    public HashMap<Integer, String> getErrors() {
-        return errors;
-    }
-
-    public ArrayList<Stack<String>> getOutStacks() {
-        return outStacks;
     }
 }
